8f7bf6da0e82fbfe24b4189a4394e9bd958b233d
Reid Moffat <18rem8@queensu.ca>
2025-07-31 13:36:41 -0400

    LICENSE template

--- src/generators.ts
+++ src/generators.ts
@@ -57,29 +57,10 @@ class Generators {
     }
 
     private createLicense = () => {
-        const license = "MIT License\n" +
-            "\n" +
-            "Copyright (c) [YEAR] [YOUR NAME]\n" +
-            "\n" +
-            "Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
-            "of this software and associated documentation files (the \"Software\"), to deal\n" +
-            "in the Software without restriction, including without limitation the rights\n" +
-            "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" +
-            "copies of the Software, and to permit persons to whom the Software is\n" +
-            "furnished to do so, subject to the following conditions:\n" +
-            "\n" +
-            "The above copyright notice and this permission notice shall be included in all\n" +
-            "copies or substantial portions of the Software.\n" +
-            "\n" +
-            "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
-            "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
-            "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" +
-            "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" +
-            "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
-            "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n" +
-            "SOFTWARE.\n";
+        const templatePath: string = path.join(__dirname, 'templates', 'LICENSE');
+        const templateContent: string = fs.readFileSync(templatePath, 'utf8');
 
-        fs.writeFileSync(this._packageDirectory + "/LICENSE", license);
+        fs.writeFileSync(this._packageDirectory + "/LICENSE", templateContent);
     }
 
     private createTodoList = () => {
